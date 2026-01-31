40438860943350f19e688701d3b5d98c770b1557
thraraujo <thgr.araujo@gmail.com>
2026-01-31 15:42:21 -0300

    make several modifications and add books

--- personal/count.sh
+++ personal/count.sh
@@ -3,4 +3,7 @@ awk -F';' '{sum += $3+0.0} END {print sum}' 2026.csv
 echo "#######"
 echo "TEACHING:"
 awk -F';' '{sum += $4} END {printf "%.2f\n", sum}' 2026.csv
+echo "#######"
+echo "PROGRAMMING AND READING:"
+awk -F';' '{sum += $5} END {printf "%.2f\n", sum}' 2026.csv
 
