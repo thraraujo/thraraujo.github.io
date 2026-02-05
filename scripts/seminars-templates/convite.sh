6383aefb6220d97747149aa30054d66044ad6dc9
thraraujo <thgr.araujo@gmail.com>
2026-02-05 16:17:51 -0300

    write script to invite speakers

--- scripts/seminars-templates/convite.sh
+++ scripts/seminars-templates/convite.sh
@@ -1,33 +1,24 @@
 #!/usr/bin/env bash
 
-read -p " Speaker's name: " NAME
-read -p " Speaker's surname: " SURNAME
-read -p " Speaker's research: " RESEARCH
-read -p " best day: " DAY
-#read -p " Speaker's gender (m or f): " GENDER
+read -p "Seminar: " SEMINAR
+
+GENDER=$(awk -F '|' "NR==$SEMINAR {print \$1}" ./speaker.org)
+NAME=$(awk -F '|' "NR==$SEMINAR {print \$2}" ./speaker.org)
+SURNAME=$(awk -F '|' "NR==$SEMINAR {print \$3}" ./speaker.org)
+RESEARCH=$(awk -F '|' "NR==$SEMINAR {print \$4}" ./speaker.org)
+DAY=$(awk -F '|' "NR==$SEMINAR {print \$5}" ./speaker.org)
 
 BASE_DIR="/home/thiago/Work-repos/thraraujo.github.io/seminars-templates/"
-touch "$NAME.tex"
-OUTPUT="$NAME.tex"
+touch "./speakers/$NAME.org"
+OUTPUT="./speakers/$NAME.org"
 
-sed "s/NAME/$NAME\ $SURNAME/
-     s/RESEARCH/$RESEARCH/
-     s/DAY/$DAY/" ./convite-ele.org > $OUTPUT
+sed "s/{GENDER}/$GENDER/
+    s/{NAME}/$NAME\ $SURNAME/
+     s/{RESEARCH}/$RESEARCH/
+     s/{DAY}/$DAY/" ./convite.org > $OUTPUT
 
 echo "-- Message --" 
 cat $OUTPUT
 echo "-- End --" 
 
 echo "This is the end..."
-
-
-
-
-
-#if [ $GENDER = m ] ; then
-#    echo $BASE_DIR
-#    # touch $NAME.tex
-#    # sed 's/NOME/$NAME/' convite-ele.tex > 
-#else
-#    echo " the speaker is called $NAME and is a woman "
-#fi
