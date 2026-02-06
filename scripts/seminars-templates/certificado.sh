e6785c054ce46b6fe9d91e318c0721421c8abdaa
thraraujo <thgr.araujo@gmail.com>
2026-02-06 03:57:13 -0300

    improve certificates and overall structure of the documents

--- scripts/seminars-templates/certificado.sh
+++ scripts/seminars-templates/certificado.sh
@@ -2,23 +2,24 @@
 
 read -p "Seminar: " SEMINAR
 
-GENDER=$(awk -F '|' "NR==$SEMINAR {print \$2}" ./speaker.org)
-NAME=$(awk -F '|' "NR==$SEMINAR {print \$3}" ./speaker.org)
-SURNAME=$(awk -F '|' "NR==$SEMINAR {print \$4}" ./speaker.org)
-DAY=$(awk -F '|' "NR==$SEMINAR {print \$6}" ./speaker.org)
-TITLE=$(awk -F '|' "NR==$SEMINAR {print \$7}" ./speaker.org)
+PRONOUN01=$(awk -F '|' "NR==$SEMINAR {print \$7}" ./speaker.org)
+TREATMENT=$(awk -F '|' "NR==$SEMINAR {print \$8}" ./speaker.org)
+NAME=$(awk -F '|' "NR==$SEMINAR {print \$2}" ./speaker.org)
+SURNAME=$(awk -F '|' "NR==$SEMINAR {print \$3}" ./speaker.org)
+DAY=$(awk -F '|' "NR==$SEMINAR {print \$1}" ./speaker.org)
+TITLE=$(awk -F '|' "NR==$SEMINAR {print \$5}" ./speaker.org)
 
 BASE_DIR="/home/thiago/Work-repos/thraraujo.github.io/seminars-templates/"
 touch "./output/$NAME.tex"
 OUTPUT="./output/$NAME.tex"
 
-sed "s/{GENDER}/$GENDER/
-    s/{NAME}/$NAME\ $SURNAME/
+sed "s/{TREATMENT}/$PRONOUN01\ $TREATMENT/
+     s/{SPEAKER}/$NAME\ $SURNAME/
      s/{TITLE}/$TITLE/
      s/{DAY}/$DAY/" ./certificado.tex > $OUTPUT
 
-echo "-- Message --" 
-cat $OUTPUT
-echo "-- End --" 
+echo "Creating pdf..." 
 
-echo "This is the end..."
+cd ./output && pdflatex $NAME.tex
+
+echo "Done!!"
--- scripts/seminars-templates/convite.sh
+++ scripts/seminars-templates/convite.sh
@@ -2,18 +2,20 @@
 
 read -p "Seminar: " SEMINAR
 
-GENDER=$(awk -F '|' "NR==$SEMINAR {print \$1}" ./speaker.org)
-NAME=$(awk -F '|' "NR==$SEMINAR {print \$3}" ./speaker.org)
-SURNAME=$(awk -F '|' "NR==$SEMINAR {print \$4}" ./speaker.org)
-RESEARCH=$(awk -F '|' "NR==$SEMINAR {print \$5}" ./speaker.org)
-DAY=$(awk -F '|' "NR==$SEMINAR {print \$6}" ./speaker.org)
+PRONOUN01=$(awk -F '|' "NR==$SEMINAR {print \$6}" ./speaker.org)
+PRONOUN02=$(awk -F '|' "NR==$SEMINAR {print \$7}" ./speaker.org)
+TREATMENT=$(awk -F '|' "NR==$SEMINAR {print \$8}" ./speaker.org)
+NAME=$(awk -F '|' "NR==$SEMINAR {print \$2}" ./speaker.org)
+SURNAME=$(awk -F '|' "NR==$SEMINAR {print \$3}" ./speaker.org)
+RESEARCH=$(awk -F '|' "NR==$SEMINAR {print \$4}" ./speaker.org)
+DAY=$(awk -F '|' "NR==$SEMINAR {print \$1}" ./speaker.org)
 
 BASE_DIR="/home/thiago/Work-repos/thraraujo.github.io/seminars-templates/"
 touch "./output/$NAME.org"
 OUTPUT="./output/$NAME.org"
 
-sed "s/{GENDER}/$GENDER/
-    s/{NAME}/$NAME\ $SURNAME/
+sed "s/{GREATINGS}/$PRONOUN01\ $TREATMENT\ $NAME\ $SURNAME/
+     s/{PRONOUN}/$PRONOUN02/
      s/{RESEARCH}/$RESEARCH/
      s/{DAY}/$DAY/" ./convite.org > $OUTPUT
 
@@ -21,4 +23,4 @@ echo "-- Message --"
 cat $OUTPUT
 echo "-- End --" 
 
-echo "This is the end..."
+echo "Done"
